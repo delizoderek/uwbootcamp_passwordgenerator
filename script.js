05dba3602a81d32a7540f0393104dcb8467522ce
Derek DeLizo <delizoderek@gmail.com>
2021-06-23 18:02:01 -0700

    Added a list to track the chosen character types. Created an object that contains all the functions for returning random character types.

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 // Assignment Code
+
+//Object contains all methods for
+let characterGenerator = {
+  specialchar: `"'!@#$%^&*()_-+=~[{|:;<,>.?\/}]`,
+  lowercase: function(){
+    let ascii = Math.floor(Math.random * 26);
+    return String.fromCharCode(97 + ascii);
+  },
+  uppercase: function(){
+    let ascii = Math.floor(Math.random * 26);
+    return String.fromCharCode(65 + ascii);
+  },
+  number: function(){
+    return Math.floor(Math.random * 10);;
+  },
+  special: function(){
+    let randIdx = Math.floor(Math.random * this.specialchar.length)
+    return this.specialchar.charAt();
+  }
+};
+
 var generateBtn = document.querySelector("#generate");
 
 // Handles the process for generating a password
 function generatePassword(){
-  // TODO: Ask for a password length
+  let charTypes = [];
+  // Ask for a password length
     // Password length needs to be between 8 and 128 characters 
     let passLen = prompt('How long do you want your password to be?');
-  // TODO: Ask if the user wants lowercase characters
-    let useLower = prompt('Do you want to use lowercase letters?');
-  // TODO: Ask if the user wants UPPERCASE characters
-    let useUpper =  prompt('Do you want to use uppercase letters?');
-  // TODO: Ask if the user wants Numbers characters
-    let useNumbers = prompt('Do you want to use numbers?');
-  // TODO: Ask if the user wants Special characters
-    let useSpecial = prompt('Do you want to use special characters?');
+  // Ask if the user wants lowercase characters
+    let useLower = confirm('Do you want to use lowercase letters?');
+    if (useLower){
+      charTypes.push('l');
+    }
+    // Ask if the user wants UPPERCASE characters
+    let useUpper =  confirm('Do you want to use uppercase letters?');
+    if (useUpper){
+      charTypes.push('u');
+    }
+    // Ask if the user wants Numbers characters
+    let useNumbers = confirm('Do you want to use numbers?');
+    if (useNumbers){
+      charTypes.push('n');
+    }
+    // Ask if the user wants Special characters
+    let useSpecial = confirm('Do you want to use special characters?');
+    if (useSpecial){
+      charTypes.push('s');
+    }
     // User must select atleast 1 character type
-  // TODO: Generate the password based on the following criteria
+  // Generate the password based on the following criteria
+    // loop from 0 to passLen - 1
+      // on each iteration
+      // A character type needs to be selected at random
+      // A sub character needs to be selected from that type group
 }
 // Write password to the #password input
 function writePassword() {
