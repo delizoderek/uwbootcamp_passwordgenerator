f38702607e187dc11265bfc23fa2964dca0c18e6
Derek DeLizo <delizoderek@gmail.com>
2021-06-24 14:47:45 -0700

    Complete the randomized portion of the password generator and edge case handling.

--- script.js
+++ script.js
@@ -1,59 +1,130 @@
 // Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+let passwordBuilder = {
+  password: "",
+  length: 0,
+  criteria: [],
+  l: [], //Tracks indexes of lowercase character
+  u: [], //Tracks indexes of character type
+  n: [], //Tracks indexes of character type
+  s: [], //Tracks indexes of character type
+  checkCriteriaMet: function(){
+    //Checks if the password has met current criteria
+    return true;
+  }
+};
 
 //Object contains all methods for
 let characterGenerator = {
   specialchar: `"'!@#$%^&*()_-+=~[{|:;<,>.?\/}]`,
-  lowercase: function(){
-    let ascii = Math.floor(Math.random * 26);
+  lowercase: function () {
+    let ascii = Math.floor(Math.random() * 26);
     return String.fromCharCode(97 + ascii);
   },
-  uppercase: function(){
-    let ascii = Math.floor(Math.random * 26);
+  uppercase: function () {
+    let ascii = Math.floor(Math.random() * 26);
     return String.fromCharCode(65 + ascii);
   },
-  number: function(){
-    return Math.floor(Math.random * 10);;
+  number: function () {
+    return Math.floor(Math.random() * 10);
   },
-  special: function(){
-    let randIdx = Math.floor(Math.random * this.specialchar.length)
-    return this.specialchar.charAt();
-  }
+  special: function () {
+    let randIdx = Math.floor(Math.random() * this.specialchar.length);
+    return this.specialchar.charAt(randIdx);
+  },
 };
 
-var generateBtn = document.querySelector("#generate");
+function getCharacter(charType){
+    switch (charType) {
+      case "l":
+        return characterGenerator.lowercase();
+      case "u":
+        return characterGenerator.uppercase();
+      case "n":
+        return characterGenerator.number();
+      case "s":
+        return characterGenerator.special();
+      default:
+        return characterGenerator.lowercase();
+    }
+}
+
+function constructPassword(numChar) {
+  // let pass = "";
+  // console.log(typeArray);
+  // TODO: Add logic to ensure all character types are added
+  for (let index = 0; index < numChar; index++) {
+    let typeArray = passwordBuilder.criteria;
+    let randType = Math.floor(Math.random() * typeArray.length);
+    passwordBuilder.password += getCharacter(typeArray[randType]);
+    passwordBuilder[typeArray[randType]].push(index);
+  }
+
+  //Check if criteria is met
+  if(passwordBuilder.checkCriteriaMet() == false){
+    //Adjust password
+  }
+  console.log(passwordBuilder);
+  return passwordBuilder.password;
+}
 
 // Handles the process for generating a password
-function generatePassword(){
+function generatePassword() {
   let charTypes = [];
-  // Ask for a password length
-    // Password length needs to be between 8 and 128 characters 
-    let passLen = prompt('How long do you want your password to be?');
-  // Ask if the user wants lowercase characters
-    let useLower = confirm('Do you want to use lowercase letters?');
-    if (useLower){
-      charTypes.push('l');
+  let lenthInRange = false;
+  let minCharTypes = 0;
+  let passLen = 0;
+  let useLower = false;
+  let useUpper = false;
+  let useNumbers = false;
+  let useSpecial = false;
+  // Password must be an integer
+  while (!lenthInRange) {
+    passLen = prompt("How long do you want your password to be?");
+    if (passLen >= 8 && passLen <= 128) {
+      lenthInRange = true;
+    } else {
+      alert(
+        "Character length is invalid. Please enter a number between 8 and 128"
+      );
+    }
+  }
+
+  // User must select atleast 1 character type
+  while (minCharTypes < 1) {
+    // Ask if the user wants lowercase characters
+    useLower = confirm("Do you want to use lowercase letters?");
+    if (useLower) {
+      charTypes.push("l");
     }
     // Ask if the user wants UPPERCASE characters
-    let useUpper =  confirm('Do you want to use uppercase letters?');
-    if (useUpper){
-      charTypes.push('u');
+    useUpper = confirm("Do you want to use uppercase letters?");
+    if (useUpper) {
+      charTypes.push("u");
     }
     // Ask if the user wants Numbers characters
-    let useNumbers = confirm('Do you want to use numbers?');
-    if (useNumbers){
-      charTypes.push('n');
+    useNumbers = confirm("Do you want to use numbers?");
+    if (useNumbers) {
+      charTypes.push("n");
     }
     // Ask if the user wants Special characters
-    let useSpecial = confirm('Do you want to use special characters?');
-    if (useSpecial){
-      charTypes.push('s');
+    useSpecial = confirm("Do you want to use special characters?");
+    if (useSpecial) {
+      charTypes.push("s");
     }
-    // User must select atleast 1 character type
+
+    if (charTypes.length >= 1) {
+      passwordBuilder.criteria = charTypes;
+      minCharTypes = charTypes.length;
+    } else {
+      alert(
+        "Did not select any character types. Please select atleast one character type"
+      );
+    }
+  }
   // Generate the password based on the following criteria
-    // loop from 0 to passLen - 1
-      // on each iteration
-      // A character type needs to be selected at random
-      // A sub character needs to be selected from that type group
+  return constructPassword(passLen);
 }
 // Write password to the #password input
 function writePassword() {
@@ -61,7 +132,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
